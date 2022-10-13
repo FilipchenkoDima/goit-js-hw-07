2bbc228091e66bf5d622229ddfe7ff9ac8ceb18a
FilipchenkoDima <blewdinor@gmail.com>
2022-10-13 12:58:34 +0300

    02-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = (galleryItems) => {
+    return galleryItems.map(item => {
+    return `<div class="gallery__item">
+  <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</div>`
+}).join('');
+};
+const finishedMarkup = galleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', finishedMarkup);
+
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
